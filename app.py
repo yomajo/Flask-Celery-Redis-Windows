af3cd61ecd4bfedb55e2a9d65fafac6dc11ac326
yomajo <ed.vinas@yahoo.com>
2022-06-14 09:21:14 +0300

    cleaned up

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from time import sleep
-
-from flask import Flask, redirect, url_for
-from celery import Celery
+from flask import Flask, url_for
 from flask_celery import make_celery
-# from celery.result import AsyncResult
 
 
 app = Flask(__name__)
@@ -11,15 +8,20 @@ app.config.update(CELERY_CONFIG={
     'broker_url': 'redis://127.0.0.1:6379',
     'result_backend': 'redis://127.0.0.1:6379',
 })
-
 celery = make_celery(app)
 
-
+# --------- TASKS ---------
 @celery.task()
 def add_together(a, b):
     return a + b
 
+@celery.task()
+def computationaly_fake():
+    sleep(20)
+    return {'msg':'LE RESULT'}
 
+
+# --------- ROUTES ---------
 @app.route('/')
 def index():
     # task = add_together.delay(a=5, b=2)
@@ -35,15 +37,6 @@ def status(task_id):
         return f'<p>Finished executing function. Result:</p><p>{task.result["msg"]}</p>'
     else:
         return f'Your task is being processed. Current status: {task.status}'
-
-@celery.task()
-def add_together(a, b):
-    return a + b
-
-@celery.task()
-def computationaly_fake():
-    sleep(20)
-    return {'msg':'LE RESULT'}
 
 
 if __name__ == '__main__':
